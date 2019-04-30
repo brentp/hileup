b72339ae6083b3223f948b244de5cb9063918dc0
Brent Pedersen <bpederse@gmail.com>
2019-04-30 11:06:08 -0600

    drop wide format and output QC file

--- src/hilepkg/rcbbc.nim
+++ src/hilepkg/rcbbc.nim
@@ -1,6 +1,9 @@
 import argparse
 import strformat
+import strutils
+import sets
 import hts/bam
+import hts/bgzf/bgzi
 import lapper
 
 include ./utils
@@ -32,31 +35,36 @@ iterator gen_start_ends(c: Cigar, ipos: int): pair {.inline.} =
     if last.stop != -1:
       yield last
 
-proc output_wide(bed:TableRef[string, Lapper[region]], order: seq[string]) =
-  var bcs_order = newSeqOfCap[string](8192)
+proc output(bed:TableRef[string, Lapper[region]], order: seq[string], output_prefix: string) =
+  var out_path = output_prefix & ".counts.bed.gz"
+  var bgz = wopen_bgzi(out_path, 0, 1, 2, true)
+  stderr.write_line &"[rcbbc] writing bgzipped long format file to {out_path}"
   for chrom in order:
     for reg in bed[chrom]:
       for bc, cnt in reg.barcodes:
-        bcs_order.add(bc)
-  sort(bcs_order)
-  echo &"""#chrom\tstart\tstop\t{join(bcs_order, "\t")}"""
-  var counts = newSeq[string](bcs_order.len)
-  for chrom in order:
-    for reg in bed[chrom]:
-      for i, bc in bcs_order:
-        counts[i] = $reg.barcodes.getOrDefault(bc, 0)
-      echo &"""{chrom}\t{reg.start}\t{reg.stop}\t{join(counts, "\t")}"""
+        discard bgz.write_interval(&"{chrom}\t{reg.start}\t{reg.stop}\t{bc}\t{cnt}", chrom, reg.start, reg.stop)
+  discard bgz.close()
 
-proc output(bed:TableRef[string, Lapper[region]], order: seq[string], wide: bool) =
-  if wide:
-    output_wide(bed, order)
-  else:
-    for chrom in order:
-      for reg in bed[chrom]:
-        for bc, cnt in reg.barcodes:
-          echo &"{chrom}\t{reg.start}\t{reg.stop}\t{bc}\t{cnt}"
+proc output_qc(bed: TableRef[string, Lapper[region]], output_prefix: string, counts_by_bc_total: CountTable[string], counts_by_bc_target: CountTable[string]) =
+  # columns are:
+  # 1. cell-barcode
+  # 2. total reads for this bar code.
+  # 3. reads from this barcode within the BED regions.
+  # 4. proportion of reads for this barcode  within the bed regions (FRiP)
+  var out_path = output_prefix & ".cb-counts.txt"
+  stderr.write_line &"[rcbbc] writing reads-per-cell and FRiP to {out_path}"
+  var fh:File
+  if not open(fh, out_path, fmWrite):
+    quit "couldn't open path for cell-barcodes at:" & out_path
 
-proc rcbbc(ibam: Bam, bed: TableRef[string, Lapper[region]], tagid: string, exclude_flags: uint16, min_mapping_quality: uint8, wide:bool) =
+  fh.write("#barcode\ttotal_reads\ton_target_reads\tfraction_on_target\n")
+  for bc in counts_by_bc_total.keys:
+    var tot = counts_by_bc_total[bc]
+    var ont = counts_by_bc_target.getOrDefault(bc)
+    fh.write(&"{bc}\t{tot}\t{ont}\t{ont.float / tot.float:.3f}\n")
+  fh.close()
+
+proc rcbbc(ibam: Bam, bed: TableRef[string, Lapper[region]], white_list: HashSet[string], tagid: string, exclude_flags: uint16, min_mapping_quality: uint8, output_prefix:string) =
 
   var last_tid = -1
   var last_tree: Lapper[region]
@@ -65,6 +73,12 @@ proc rcbbc(ibam: Bam, bed: TableRef[string, Lapper[region]], tagid: string, excl
   var total = 0
   var notag = 0
   var added = 0
+  var non_white_list = 0
+  var hasWhiteList = white_list.len > 0
+
+  var counts_by_bc_total = initCountTable[string](8192)
+  var counts_by_bc_target = initCountTable[string](8192)
+
 
   for aln in ibam:
     total.inc
@@ -90,15 +104,31 @@ proc rcbbc(ibam: Bam, bed: TableRef[string, Lapper[region]], tagid: string, excl
       notag += 1
     else:
       btag = $bc.get
+    if hasWhiteList and btag notin white_list:
+      non_white_list.inc
+      continue
 
+    var on_target = false
+    counts_by_bc_total.inc(btag)
     for p in gen_start_ends(aln.cigar, aln.start):
-      last_tree.each_seek(p.start, p.stop, proc(r:region) = r.barcodes.inc(btag))
+      last_tree.each_seek(p.start, p.stop, proc(r:region) = (r.barcodes.inc(btag); on_target = true))
+    if on_target:
+      counts_by_bc_target.inc(btag)
     added.inc
 
-  stderr.write_line(&"[rcbbc] added {added} alignments out of {total}; of those {notag} had not tag for {tagid}")
+  stderr.write_line(&"[rcbbc] added {added} alignments out of {total}; of those {notag} had no tag for {tagid}")
+  if hasWhiteList:
+    stderr.write_line(&"[rcbbc] {non_white_list} alignments skipped because they were not present in the specified white-list")
 
-  bed.output(order, wide)
+  bed.output(order, output_prefix)
+  bed.output_qc(output_prefix, counts_by_bc_total, counts_by_bc_target)
 
+
+proc read_white_list(path: string): HashSet[string] =
+  result = initHashSet[string](8192)
+  if path == "": return
+  for l in path.lines:
+    result.incl(l.strip())
 
 proc main() =
 
@@ -107,11 +137,17 @@ proc main() =
     option("-f", "--fasta", help="path to fasta. required only for CRAM")
     option("-e", "--exclude", help="exclude alignments with any of these bits set", default="1796")
     option("-m", "--min-mapping-quality", help="exclude alignments with a mapping-quality below this", default="1")
-    option("-t", "--tag", default="CB", help="tag on which to partigion read-counts")
-    flag("-w", "--wide", help="output counts in wide form (1 column per barcode) default is long form as this is usually sparse.")
+    option("-w", "--white-list", help="line-delimited list of barcodes to include. default is to include any seen in the bam")
+    option("-t", "--tag", default="CB", help="tag on which to partition read-counts")
+    option("-p", "--prefix", default="rcbbc", help="output prefix for files written by rcbbc")
     arg("bed")
     arg("bam")
 
+  try:
+    discard p.parse()
+  except UsageError:
+    echo p.help
+    quit 2
   var opts = p.parse()
   if opts.help:
     quit 2
@@ -127,7 +163,11 @@ proc main() =
     echo p.help()
     quit "[rcbbc] tag must be length 2"
 
-  ibam.rcbbc(bed, opts.tag, parseInt(opts.exclude).uint16, parseInt(opts.min_mapping_quality).uint8, opts.wide)
+  var white_list = read_white_list(opts.white_list)
+  if opts.prefix == "rcbbc":
+    stderr.write_line &"[rcbbc] outputting files with prefix {opts.prefix}; use --prefix to change this"
+
+  ibam.rcbbc(bed, white_list, opts.tag, parseInt(opts.exclude).uint16, parseInt(opts.min_mapping_quality).uint8, opts.prefix)
 
 
 when isMainModule:
