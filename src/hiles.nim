88fe4b3fb7ce0b85e3783a75bd86f7d06b2941a2
Brent Pedersen <bpederse@gmail.com>
2019-05-03 09:23:24 -0600

    commit nim work

--- src/hiles.nim
+++ src/hiles.nim
@@ -28,7 +28,8 @@ type HileSite* = object
 
 type QnameAt = object
   qname_i: uint32
-  position: uint32
+  # index into the hiles site objects
+  hiles_i: uint32
 
 type Qnames = object
   qnames_u2s: TableRef[uint32, string]
@@ -52,10 +53,55 @@ type Hiles* = object
   ## qnames are optionally stored externally
   refOffset*: uint32
   # allow finding id by name and name by id. since every base in the read has a hilesite
-  # we avoid storing the read-name more than needed.
+  # we avoid storing the read-name more than needed. even with a shallow
+  # version, or a pointer in C, we save 32 bits.
   qnames: Qnames
   # a qnameat allows us to get the exact base, read combination where the
   # insertion /deletion occurred.
   insertions: TableRef[QnameAt, seq[insertion]]
   deletions: TableRef[QnameAt, seq[deletion]]
   hiles*: seq[HileSite]
+
+import binaryheap
+import hts/bam
+import hts/fai
+
+type Piece* = object
+  start: uint32
+  qname_i: uint32
+  len: uint16
+  op: uint8
+  mapq: uint8
+  #baseq: seq[uint8]
+
+template stop(p:Piece): int =
+  return p.start + p.len
+
+iterator pieces(aln:Record, reference:var string, read_seq: var string): Piece = 
+  #var bqs = cast[seq[uint8]](bam_get_qual(aln.b))
+  discard aln.sequence(read_seq)
+  # TODO: start here
+
+iterator heapify(ibam:Bam, fai:Fai, chrom:string, min_mapping_quality:uint8, min_base_quality:uint8): Piece =
+
+  var reference = fai.get(chrom, 0, fai.chrom_len(chrom))
+  var read_seq:string
+  var h = newHeap[Piece]() do (a, b: Piece) -> int:
+    return a.start.int - b.start.int
+
+  for aln in ibam.query(chrom):
+    if aln.mapping_quality < min_mapping_quality: continue
+    while h.size > 0 and h.peek.start < aln.start.uint32:
+      yield h.pop
+
+    for piece in aln.pieces(reference, read_seq):
+      h.push(piece)
+
+  while h.size > 0:
+    yield h.pop
+
+
+
+
+when isMainModule:
+  echo sizeof(Piece())
